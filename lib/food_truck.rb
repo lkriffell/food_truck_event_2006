31777ddb247d6e2024b4529789dc1668567d8549
Logan Riffell <lkriffell@gmail.com>
2020-08-05 09:39:13 -0600

    Add potential revenue method

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -12,7 +12,16 @@ class FoodTruck
     @inventory[item]
   end
 
-  def stock(item, amount)
-    @inventory[item] = amount
+  def stock(item, quantity)
+    @inventory[item] = quantity
   end
+
+  def potential_revenue
+    potential = 0
+    @inventory.each do |item, quantity|
+      potential += item.price[1..-1].to_f * quantity
+    end
+    potential
+  end
+
 end
