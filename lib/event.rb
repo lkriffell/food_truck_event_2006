05049717bbfc2c57882d57672cbfa1d047111258
Logan Riffell <lkriffell@gmail.com>
2020-08-05 10:24:21 -0600

    Add sorted item list

--- lib/event.rb
+++ lib/event.rb
@@ -25,4 +25,37 @@ class Event
       end
     end.compact
   end
+
+  # def total_inventory
+  #   all_inventory = Hash.new()
+  #   @food_trucks.each do |truck|
+  #     truck.inventory.each do |item, amount|
+  #       add_to_total_inventory(all_inventory, item, amount, truck)
+  #     end
+  #   end
+  #   all_inventory
+  # end
+  #
+  # def add_to_total_inventory(all_inventory, item, amount, truck)
+  #   if all_inventory[item] == nil
+  #     all_inventory[item] = Hash.new(0)
+  #   else
+  #     all_inventory[item][:quantity] += amount
+  #     if all_inventory[item][:food_trucks] == 0
+  #       all_inventory[item][:food_trucks] = [truck]
+  #     else
+  #       all_inventory[item][:food_trucks] << truck
+  #     end
+  #   end
+  # end
+
+  def sorted_item_list
+    item_list = []
+    @food_trucks.each do |truck|
+      truck.inventory.keys.each do |item|
+        item_list << item.name
+      end
+    end
+    item_list.uniq.sort
+  end
 end
